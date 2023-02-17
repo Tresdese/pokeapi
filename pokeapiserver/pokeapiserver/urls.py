778db4f2b0dcb914180dafe87f2b3c04afcfe816
Tresdese <super_diegobros@hotmail.com>
2023-02-17 12:15:13 -0600

    ola

--- pokeapiserver/pokeapiserver/urls.py
+++ pokeapiserver/pokeapiserver/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pokeapiserver.views import saludo, adios
+from .views import saludo, adios
 
 urlpatterns = [
     path('admin/', admin.site.urls),
